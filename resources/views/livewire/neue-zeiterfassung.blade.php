bd43f99ddc789c8b8400eebdf0bb15076b9b2f41
Mattheus <mattheus@gmail.com>
2026-03-03 15:41:36 +0100

    Added Tailwind styling to the input

--- resources/views/livewire/neue-zeiterfassung.blade.php
+++ resources/views/livewire/neue-zeiterfassung.blade.php
@@ -1,5 +1,19 @@
 <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
     <div class="p-6">
+        <form action="">
+            <label class="block text-sm/6 font-medium text-gray-900 dark:text-white">Datum des Arbeitstages</label>
+            <div class="mt-2">
+                <div
+                    class="flex items-center rounded-md bg-white pl-3 outline-1 -outline-offset-1 outline-gray-300 has-[input:focus-within]:outline-2 has-[input:focus-within]:-outline-offset-2 has-[input:focus-within]:outline-indigo-600 dark:bg-white/5 dark:outline-gray-600 dark:has-[input:focus-within]:outline-indigo-500">
+                    <input type="date"
+                        class="block min-w-0 grow py-1.5 pr-3 pl-1 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm/6 dark:bg-gray-800 dark:text-white dark:placeholder:text-gray-500" />
+                </div>
+            </div>
+            <input type="time">Startzeit<input>
+            <input type="time">Endzeit<input>
+            <input type="number" maxlength="479">Mittagspause<input>
+            <input type="time">Soll Arbeitszeit<input>
+        </form>
 
 
     </div>
