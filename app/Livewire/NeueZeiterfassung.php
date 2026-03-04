7eea63d22fd4fa47d2118b6e1f8e986224543fe1
Mattheus <mattheus@gmail.com>
2026-03-04 11:16:16 +0100

    Added Validation + Styling and new elements

--- app/Livewire/NeueZeiterfassung.php
+++ app/Livewire/NeueZeiterfassung.php
@@ -3,10 +3,53 @@
 namespace App\Livewire;
 
 use Livewire\Attributes\Title;
+use Livewire\Attributes\Rule;
+use App\Models\Zeiterfassung;
 use Livewire\Component;
 
 #[Title('Neue-Zeiterfassung')] class NeueZeiterfassung extends Component
 {
+    #[Rule('required|min:1')]
+    public $Name;
+
+    #[Rule('required')]
+    public $Kategorie;
+
+    #[Rule('required|date_format:Y-m-d|before_or_equal:today')]
+    public $Datum;
+
+    #[Rule('required')]
+    public $Startzeit;
+
+    #[Rule('required')]
+    public $Endzeit;
+
+    #[Rule('required')]
+    public $Soll_Arbeitszeit;
+
+    #[Rule('required|Integer')]
+    public $Mittagsapause;
+    
+    #[Rule('max:100')]
+    public $Notiz;
+
+    public function add_data_to_db()
+    {
+        Zeiterfassung::create([
+            'Name' => $this->Name,
+            'Datum' => $this->Datum,
+            'Kategorie' => $this->Kategorie,
+            'Arbeitsbeginn' => $this->Startzeit,
+            'Arbeitsende' => $this->Endzeit,
+            'Mittagspause' => $this->Mittagsapause,
+            'Arbeitszeit' => '08:00',
+            'Soll_Arbeitszeit' => $this->Soll_Arbeitszeit,
+            'Ueberstunden_Minusstunden' => '00:00',
+            'Notiz' => $this->Notiz,
+        ]);
+    }
+
+
     public function render()
     {
         return view('livewire.neue-zeiterfassung');
--- resources/views/livewire/neue-zeiterfassung.blade.php
+++ resources/views/livewire/neue-zeiterfassung.blade.php
@@ -1,20 +1,105 @@
 <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
     <div class="p-6">
-        <form action="">
-            <label class="block text-sm/6 font-medium text-gray-900 dark:text-white">Datum des Arbeitstages</label>
+        <form wire:submit="">
             <div class="mt-2">
+                <label class="block text-sm/6 font-medium text-gray-900 dark:text-white">Name</label>
                 <div
                     class="flex items-center rounded-md bg-white pl-3 outline-1 -outline-offset-1 outline-gray-300 has-[input:focus-within]:outline-2 has-[input:focus-within]:-outline-offset-2 has-[input:focus-within]:outline-indigo-600 dark:bg-white/5 dark:outline-gray-600 dark:has-[input:focus-within]:outline-indigo-500">
-                    <input type="date"
+                    <input type="text"
                         class="block min-w-0 grow py-1.5 pr-3 pl-1 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm/6 dark:bg-gray-800 dark:text-white dark:placeholder:text-gray-500" />
                 </div>
             </div>
-            <input type="time">Startzeit<input>
-            <input type="time">Endzeit<input>
-            <input type="number" maxlength="479">Mittagspause<input>
-            <input type="time">Soll Arbeitszeit<input>
+
+            <div class="mb-10 mt-4">
+                <div
+                    class="flex items-center rounded-md bg-white pl-3 outline-1 -outline-offset-1 outline-gray-300 has-[input:focus-within]:outline-2 has-[input:focus-within]:-outline-offset-2 has-[input:focus-within]:outline-indigo-600 dark:bg-white/5 dark:outline-gray-600 dark:has-[input:focus-within]:outline-indigo-500">
+                    <select wire:model.live='Kategorie'
+                        class="block min-w-0 grow py-1.5 pr-3 pl-1 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm/6 dark:bg-gray-800 dark:text-white dark:placeholder:text-gray-500">
+                        <option hidden readonly selected> -- select an options -- </option>
+
+                        <option class="text-white ">
+                            Arbeitstag
+                        </option>
+                        <option class="text-white">
+                            Urlaub
+                        </option>
+                        <option class="text-white">
+                            Krankenstand
+                        </option>
+                    </select>
+                </div>
+            </div>
+            @if ($Kategorie == 'Arbeitstag')
+                <div class="mt-2">
+                    <label class="block text-sm/6 font-medium text-gray-900 dark:text-white">Datum des
+                        Arbeitstages</label>
+                    <div
+                        class="flex items-center rounded-md bg-white pl-3 outline-1 -outline-offset-1 outline-gray-300 has-[input:focus-within]:outline-2 has-[input:focus-within]:-outline-offset-2 has-[input:focus-within]:outline-indigo-600 dark:bg-white/5 dark:outline-gray-600 dark:has-[input:focus-within]:outline-indigo-500">
+
+                        <input type="date"
+                            class="block min-w-0 grow py-1.5 pr-3 pl-1 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm/6 dark:bg-gray-800 dark:text-white dark:placeholder:text-gray-500" />
+                    </div>
+                </div>
+
+                <div class="mt-2">
+                    <label class="block text-sm/6 font-medium text-gray-900 dark:text-white">Startzeit</label>
+                    <div
+                        class="flex items-center rounded-md bg-white pl-3 outline-1 -outline-offset-1 outline-gray-300 has-[input:focus-within]:outline-2 has-[input:focus-within]:-outline-offset-2 has-[input:focus-within]:outline-indigo-600 dark:bg-white/5 dark:outline-gray-600 dark:has-[input:focus-within]:outline-indigo-500">
+                        <input type="time"
+                            class="block min-w-0 grow py-1.5 pr-3 pl-1 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm/6 dark:bg-gray-800 dark:text-white dark:placeholder:text-gray-500" />
+                    </div>
+                </div>
+                <div class="mt-2">
+                    <label class="block text-sm/6 font-medium text-gray-900 dark:text-white">Endzeit</label>
+                    <div
+                        class="flex items-center rounded-md bg-white pl-3 outline-1 -outline-offset-1 outline-gray-300 has-[input:focus-within]:outline-2 has-[input:focus-within]:-outline-offset-2 has-[input:focus-within]:outline-indigo-600 dark:bg-white/5 dark:outline-gray-600 dark:has-[input:focus-within]:outline-indigo-500">
+                        <input type="time"
+                            class="block min-w-0 grow py-1.5 pr-3 pl-1 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm/6 dark:bg-gray-800 dark:text-white dark:placeholder:text-gray-500" />
+                    </div>
+                </div>
+                <div class="mt-2">
+                    <label class="block text-sm/6 font-medium text-gray-900 dark:text-white">Soll_Arbeitszeit</label>
+                    <div
+                        class="flex items-center rounded-md bg-white pl-3 outline-1 -outline-offset-1 outline-gray-300 has-[input:focus-within]:outline-2 has-[input:focus-within]:-outline-offset-2 has-[input:focus-within]:outline-indigo-600 dark:bg-white/5 dark:outline-gray-600 dark:has-[input:focus-within]:outline-indigo-500">
+                        <input type="time"
+                            class="block min-w-0 grow py-1.5 pr-3 pl-1 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm/6 dark:bg-gray-800 dark:text-white dark:placeholder:text-gray-500" />
+                    </div>
+                </div>
+                <div class="mt-2">
+                    <label class="block text-sm/6 font-medium text-gray-900 dark:text-white">Mittagspause</label>
+                    <div
+                        class="flex items-center rounded-md bg-white pl-3 outline-1 -outline-offset-1 outline-gray-300 has-[input:focus-within]:outline-2 has-[input:focus-within]:-outline-offset-2 has-[input:focus-within]:outline-indigo-600 dark:bg-white/5 dark:outline-gray-600 dark:has-[input:focus-within]:outline-indigo-500">
+                        <input type="text"
+                            class="block min-w-0 grow py-1.5 pr-3 pl-1 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm/6 dark:bg-gray-800 dark:text-white dark:placeholder:text-gray-500" />
+                    </div>
+                </div>
+                
+            @elseif ($Kategorie == 'Urlaub' || $Kategorie == 'Krankenstand')
+                <div class="mt-2">
+                    <label class="block text-sm/6 font-medium text-gray-900 dark:text-white">Datum</label>
+                    <div
+                        class="flex items-center rounded-md bg-white pl-3 outline-1 -outline-offset-1 outline-gray-300 has-[input:focus-within]:outline-2 has-[input:focus-within]:-outline-offset-2 has-[input:focus-within]:outline-indigo-600 dark:bg-white/5 dark:outline-gray-600 dark:has-[input:focus-within]:outline-indigo-500">
+
+                        <input type="date"
+                            class="block min-w-0 grow py-1.5 pr-3 pl-1 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm/6 dark:bg-gray-800 dark:text-white dark:placeholder:text-gray-500" />
+                    </div>
+                </div>
+                <div class="mt-2">
+                    <label class="block text-sm/6 font-medium text-gray-900 dark:text-white">Notiz</label>
+                    <div
+                        class="flex items-center rounded-md bg-white pl-3 outline-1 -outline-offset-1 outline-gray-300 has-[input:focus-within]:outline-2 has-[input:focus-within]:-outline-offset-2 has-[input:focus-within]:outline-indigo-600 dark:bg-white/5 dark:outline-gray-600 dark:has-[input:focus-within]:outline-indigo-500">
+
+                        <textarea rows="4" cols="50"
+                            class="block min-w-0 grow py-1.5 pr-3 pl-1 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm/6 dark:bg-gray-800 dark:text-white dark:placeholder:text-gray-500"></textarea>
+                    </div>
+                </div>
+            @endif
+
+            <button type="submit"
+                class="mt-6 cursor-pointer flex w-full justify-center rounded-md bg-gray-800 px-3 py-1.5 text-sm/6 font-semibold
+         text-white shadow-xs hover:bg-gray-700 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-600">
+                Neue Zeiterfassung erstellen
+            </button>
         </form>
-
-
     </div>
 </div>
